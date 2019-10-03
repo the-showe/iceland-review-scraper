c41539a3facc8f8a684da3010170fd6b43a2ed1a
Stephen Howe <mrstephenhowe@gmail.com>
2019-10-03 11:22:40 +0200

    Ability to pull a random review.

--- __main__.py
+++ __main__.py
@@ -2,8 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 import random
 from lxml import etree
+import re
 
 SITEMAP_URL = 'https://www.iceland.co.uk/sitemap_1-product.xml'
+
 
 class IcelandReviewScraper(object):
     def __init__(self, sitemap_url):
@@ -25,35 +27,58 @@ class IcelandReviewScraper(object):
         product_url_list = self.extract_product_urls_from_sitemap()
         return random.choice(product_url_list)
 
-    def extract_review_list_from_product_page(self, product_url):
-        product_page_content = requests.get(product_url).content
-        product_page_soup = BeautifulSoup(product_page_content, parser='lxml',
-                                          features='lxml')
+    @staticmethod
+    def extract_review_list_from_product_page(product_page_soup):
         review_list_soup = product_page_soup.find('div', {'class': 'reviewList'})
         reviews = review_list_soup.find_all('div', {'class': 'feefoReview'})
         if not reviews:
-            print('no reviews :(')
-            exit()
+            return None
         return reviews
+
+    @staticmethod
+    def extract_review_from_soup(review_soup, product_url, product_title):
+        stars = review_soup.find('p', {'class': 'stars'}).find_all(
+            'svg', {'class': 'icon review-star-fill svg-review-star-fill-ems'})
+        submitted_text = review_soup.find('p', {'class': 'text-muted submitted'}).text
+        submitted_by = re.match('^Submitted by (.*) on (.*)$', submitted_text)
+        try:
+            submitter = submitted_by.groups()[0]
+            date = submitted_by.groups()[1]
+        except AttributeError as e:
+            raise AttributeError(f'{submitted_text}\n{e}')
+
+        review = {
+            'product_url': product_url,
+            'product_title': product_title,
+            'date': date,
+            'stars': len(stars),
+            'submitter': submitter,
+            'text': review_soup.find_all('p')[-1].text
+        }
+
+        review['characters'] = len(review['text'])
+        return review
 
     def get_random_review(self):
         product_url = self.get_random_product_url()
-        print(product_url)
-        reviews = self.extract_review_list_from_product_page(product_url)
+        product_page_content = requests.get(product_url).content
+        product_page_soup = BeautifulSoup(product_page_content, parser='lxml',
+                                          features='lxml')
+        product_title = product_page_soup.find('h2', {'class': 'product-name'}).text
+        reviews = self.extract_review_list_from_product_page(product_page_soup)
+        if not reviews:
+            return None
         review_soup = random.choice(reviews)
-        stars = review_soup.find('p', {'class': 'stars'}).find_all('svg', {'class': 'icon review-star-fill svg-review-star-fill-ems'})
 
-        review = {
-            'stars': len(stars),
-            'submitted': review_soup.find('p', {'class': 'text-muted submitted'}).text,
-            'text': review_soup.find_all('p')[-1].text
-        }
+        review = self.extract_review_from_soup(review_soup, product_url, product_title)
 
         return review
 
 
-
 if __name__ == '__main__':
     scraper = IcelandReviewScraper(SITEMAP_URL)
-    review = scraper.get_random_review()
-    print(review)
+    review = None
+    while not review:
+        review = scraper.get_random_review()
+    for k, v in review.items():
+        print(f'{k}: {v}')
