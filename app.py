aaf34b5771b50bbd9544ba9587a3ff099a966e98
Stephen Howe <mrstephenhowe@gmail.com>
2019-10-25 16:18:46 +0200

    App returns a new review on reload.

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from scraper import IcelandReviewScraper
+from flask import Flask
+
+
+app = Flask(__name__)
 
 SITEMAP_URL = 'https://www.iceland.co.uk/sitemap_1-product.xml'
 
-if __name__ == '__main__':
+
+@app.route('/')
+def random_review():
     scraper = IcelandReviewScraper(SITEMAP_URL)
     review = None
     while not review:
         review = scraper.get_random_review()
-    for k, v in review.as_dict().items():
-        print(f'{k}: {v}')
+    return '<br/>'.join(f'{k}: {v}' for k, v in review.as_dict().items())
+
+if __name__ == '__main__':
+    app.run()
