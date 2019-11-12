13ae817b0743ebfca3738cbd00b07bdf3a05b459
Stephen Howe <stephen.howe@crealytics.com>
2019-11-12 14:36:29 +0100

    Ensure same review is never tweeted twice.

--- __main__.py
+++ __main__.py
@@ -16,11 +16,23 @@ def find_random_review():
     return product_page, review
 
 
-if __name__ == '__main__':
-    product_page, review = find_random_review()
+def write_review_tweet(product_page, review):
     review_parts = [product_page.product_title, review.star_str, review.text]
     if product_page.product_price != 'N/A':
         review_parts.insert(1, product_page.product_price)
-    review_str = ' - '.join(review_parts)
+    return ' - '.join(review_parts)
+
+
+if __name__ == '__main__':
     twitter_account = TwitterAccount()
-    twitter_account.tweet_image(product_page.product_image_url, review_str)
+    previous_tweet_strings = (t.text for t in twitter_account.tweets)
+
+    # Set up while loop to ensure review hasn't been tweeted before
+    tweeted_before = True
+    while tweeted_before:
+        product_page, review = find_random_review()
+        tweet_string = write_review_tweet(product_page, review)
+        tweeted_before = tweet_string in previous_tweet_strings
+    else:
+        twitter_account.tweet_image(
+            product_page.product_image_url, tweet_string)
