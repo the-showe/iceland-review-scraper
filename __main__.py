c4049f52dba867e1f1f676fa0331f5abddb4d7e2
Stephen Howe <stephen.howe@crealytics.com>
2019-11-12 10:59:54 +0100

    Functioning twitter bot.

---
+++ __main__.py
@@ -0,0 +1,24 @@
+from scraper import IcelandReviewScraper
+from twitter import TwitterAccount
+
+SITEMAP_URL = 'https://www.iceland.co.uk/sitemap_1-product.xml'
+
+
+def find_random_review():
+    scraper = IcelandReviewScraper(SITEMAP_URL)
+    review = None
+    while not review:
+        product_page = scraper.get_random_product_page()
+        if not product_page.has_reviews:
+            continue
+        review = product_page.get_random_review()
+    review.star_str = "⭐" * review.num_stars
+    return product_page, review
+
+
+if __name__ == '__main__':
+    product_page, review = find_random_review()
+    review_parts = [product_page.product_title, review.star_str, review.text]
+    review_str = ' - '.join(review_parts)
+    twitter_account = TwitterAccount()
+    twitter_account.tweet_image(product_page.product_image_url, review_str)
---
+++ twitter.py
@@ -0,0 +1,32 @@
+import requests
+import tweepy
+import os
+
+
+class TwitterAccount(object):
+    def __init__(self):
+        auth = tweepy.OAuthHandler(os.environ['TWITTER_API_KEY'],
+                                   os.environ['TWITTER_API_SECRET'])
+
+        auth.set_access_token(os.environ['TWITTER_API_ACCESS_TOKEN'],
+                              os.environ['TWITTER_API_ACCESS_TOKEN_SECRET'])
+
+        self.api = tweepy.API(auth)
+        self.account_name = 'IcelandReviews'
+
+    @property
+    def user(self):
+        return self.api.get_user(self.account_name)
+
+    def tweet_image(self, url, message):
+        filename = 'temp.jpg'
+        request = requests.get(url, stream=True)
+        if request.status_code == 200:
+            with open(filename, 'wb') as image:
+                for chunk in request:
+                    image.write(chunk)
+
+            self.api.update_with_media(filename, status=message)
+            os.remove(filename)
+        else:
+            raise Exception('Could not download image, so nothing to tweet.')
